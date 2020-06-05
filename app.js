6641b77cb916d324d9a8978a1fdcc5601410af4b
Tereh <lositsky@outlook.com>
2020-06-05 10:10:47 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+//alert("Я JavaScript із файлу app.js!");
+
+// Объявите две переменные: admin и name.
+// Запишите строку "Джон" в переменную name.
+// Скопируйте значение из переменной name в admin.
+// Выведите на экран значение admin, используя функцию alert(должна показать «Джон»).
+// let admin;
+// let name;
+// name = "Джон";
+// admin = name;
+
+// alert(admin);
+// let name = "Иван";
+
+// // // Вставим переменную
+// alert(`Привет, ${name}!`); // Привет, Иван!
+
+// //Вставим выражение
+alert(`результат: ${1 + 2}`); // результат: 3
+
+let nameUser = "Ilya";
+
+// выражение - число 1
+alert(`hello ${1}`); // hello 1
+
+// выражение - строка "name"
+alert(`hello ${"name"}`); // hello name
+
+// выражение - переменная, вставим её в строку
+alert(`hello ${name}`); // hello Ilya
+alert(" \t \n" - 2); // true
+alert(Number(undefined)); // пробел это тоже true (любая непу
---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+//alert("Привіт");
+// const result = confirm("В тебе гарний настрій!");
+// console.log(result);
+// alert(result);
+// const answer = +prompt("Скільки вам років?", "");
+// alert(answer + 1);
+// alert(typeof answer);
+
+//
+
+const category = "admin";
+
+console.log(`http://palmira.ck.ua/${category}`);
+
+const user = "Володимир";
+alert(`Привіт, ${user}`);
